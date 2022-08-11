e1bb4cc71b819711c685d04ed6e30fde1f0cfc2c
amolmane7471 <maneamol3233@gmail.com>
2022-08-11 08:10:32 +0530

    [Amol]:added a UC1 for employee attendance

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -4,6 +4,13 @@ public class EmpWage {
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to Employee Wage Computation Pgm");
+		int IS_FULLTIME=1;
+		double empcheck=Math.floor(Math.random()*10)%2;
+		if(empcheck==IS_FULLTIME)
+			System.out.println("Emp is present");
+		else 
+			System.out.println("Emp is absent");
+
 	}
 
 }
